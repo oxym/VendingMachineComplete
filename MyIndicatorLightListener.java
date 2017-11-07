fdd7744d35ea1a81735c0abb29aec0a4a8f55bb0
Michael Han <32470787+oxym@users.noreply.github.com>
2017-11-07 09:14:22 -0600

    Add files via upload

---
+++ MyIndicatorLightListener.java
@@ -0,0 +1,29 @@
+package org.lsmr.vending.hardware;
+
+public class MyIndicatorLightListener implements IndicatorLightListener {
+
+	@Override
+	public void enabled(AbstractHardware<? extends AbstractHardwareListener> hardware) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void disabled(AbstractHardware<? extends AbstractHardwareListener> hardware) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void activated(IndicatorLight light) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void deactivated(IndicatorLight light) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
