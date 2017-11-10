37683128a94f3b917311c6cbeace292c9561e195
lukeatmycode <luke.kissick1@ucalgary.ca>
2017-11-09 18:29:47 -0700

    Actual Event Writer update
    Didnt work with last commit, this should actually fix it

--- src/ca/ucalgary/seng300/a2/EventWriter.java
+++ src/ca/ucalgary/seng300/a2/EventWriter.java
@@ -9,7 +9,6 @@ import java.util.*;
 
 public class EventWriter {
 	private BufferedWriter writer;
-	private Calendar cal = Calendar.getInstance();
     private SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
 	public EventWriter(String file) {
 		 try {
