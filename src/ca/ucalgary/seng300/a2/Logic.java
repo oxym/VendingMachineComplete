2f8e754b2c82195668af06c0aaf127baed293acf
lukeatmycode <luke.kissick1@ucalgary.ca>
2017-11-09 17:36:29 -0700

    deleted pop listener, duplicate of PCRlistener. Updated logic.

--- src/ca/ucalgary/seng300/a2/Logic.java
+++ src/ca/ucalgary/seng300/a2/Logic.java
@@ -11,15 +11,15 @@ public class Logic {
 	private int credit;
 	
 	//listeners
-	private ButtonListener buttonListener;
+	//private ButtonListener buttonListener;
+	//private PCRListener popCanRackListener;
 	private CReturnListener returnListener;
 	private CSlotListener slotListener;
 	private DeliveryListener deliveryListener;
 	private MyDisplayListener displayListener;
 	private MyIndicatorLightListener exactChangeListener;
 	private MyIndicatorLightListener outOfOrderListener;
-	private PCRListener popCanRackListener;
-	private PopListener popListener;
+	
 	private ReceptacleListener receptacleListener;
 	
 	//constructor for logic class
@@ -29,30 +29,29 @@ public class Logic {
 		ew = write;
 		credit = 0;
 		
-		buttonListener = new ButtonListener(vm, ew, this);
+		//buttonListener = new ButtonListener(vm, ew, this);
+		//popCanRackListener = new PCRListener(vm, ew, this);
 		returnListener = new CReturnListener(vm, ew, this);
 		slotListener = new CSlotListener(vm, ew, this);
 		deliveryListener = new DeliveryListener(vm, ew, this);
 		displayListener = new MyDisplayListener(vm, ew, this);
 		exactChangeListener = new MyIndicatorLightListener(vm, ew, this);
 		outOfOrderListener = new MyIndicatorLightListener(vm, ew, this);
-		popCanRackListener = new PCRListener(vm, ew, this);
-		popListener = new PopListener(vm, ew, this);
 		receptacleListener = new ReceptacleListener(vm, ew, this);
 		
 		// Register the listeners to their respective classes 
 		vm.getCoinReceptacle().register(receptacleListener);
 		vm.getDisplay().register(displayListener);
 		vm.getCoinSlot().register(slotListener);
-		
-		//below line is giving NullPointer exception
-		//vm.getCoinReturn().register(returnListener);
-		
+		vm.getCoinReturn().register(returnListener);
 		vm.getExactChangeLight().register(exactChangeListener);
 		vm.getOutOfOrderLight().register(outOfOrderListener);
 		vm.getDeliveryChute().register(deliveryListener);
+		
+		//below line is giving NullPointer exception
+		//vm.getCoinReturn().register(returnListener);
 
-		for (int i = 0; i < 6; i++) {
+		for (int i = 0; i < vm.getNumberOfPopCanRacks(); i++) {
 			vm.getPopCanRack(i).register(new PCRListener(vm, ew, this));
 			vm.getSelectionButton(i).register(new ButtonListener(vm, ew, this));
 		}
@@ -71,6 +70,10 @@ public class Logic {
 	
 	public int getCredit() {
 		return credit;
+	}
+	
+	public void changeCredit(int amount) {
+		credit =+ amount;
 	}
 
 	
