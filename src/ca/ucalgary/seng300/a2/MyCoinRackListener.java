26e2ef6f79bb704e1e06236d25b181806058e194
Michael <xiangyu.han@ucalgry.ca>
2017-11-14 20:41:41 -0600

    Updated CoinRackListener

--- src/ca/ucalgary/seng300/a2/MyCoinRackListener.java
+++ src/ca/ucalgary/seng300/a2/MyCoinRackListener.java
@@ -30,8 +30,7 @@ public class MyCoinRackListener implements CoinRackListener {
 
 	@Override
 	public void coinsFull(CoinRack rack) {
-		// TODO Auto-generated method stub
-
+		vm.getOutOfOrderLight().activate();
 	}
 
 	@Override
