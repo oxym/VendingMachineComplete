62a52b215c89fb6ef217850f60a3f862315f338c
Grimsly <xmeng.low@gmail.com>
2017-11-14 17:24:21 -0700

    Added different light listeners

---
+++ src/ca/ucalgary/seng300/a2/OutOfOrderLightListener.java
@@ -0,0 +1,34 @@
+package ca.ucalgary.seng300.a2;
+
+import org.lsmr.vending.hardware.AbstractHardware;
+import org.lsmr.vending.hardware.AbstractHardwareListener;
+import org.lsmr.vending.hardware.IndicatorLight;
+import org.lsmr.vending.hardware.IndicatorLightListener;
+
+public class OutOfOrderLightListener implements IndicatorLightListener  {
+
+	@Override
+	public void enabled(AbstractHardware<? extends AbstractHardwareListener> hardware) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void disabled(AbstractHardware<? extends AbstractHardwareListener> hardware) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void activated(IndicatorLight light) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deactivated(IndicatorLight light) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
